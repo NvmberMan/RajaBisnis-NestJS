83953a035aa88be04298f0e6405eb1da8b9ebb34
NumberMan <numberman3250@gmail.com>
2023-09-12 11:51:09 +0700

    fix cors

--- RajaBisnis-Frontend/src/content/test.tsx
+++ RajaBisnis-Frontend/src/content/test.tsx
@@ -1,52 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { json } from "react-router-dom";
-export type dummy = {
-  brand: string;
-  category: string;
-  description: string;
-  id: number;
-  price: number;
-  stock: number;
-  thumbnail: string;
-  title: string;
-};
+
 
 export default function Test() {
-  const [data, setData] = useState<dummy[]>([]);
-  const url = "'https://dummyjson.com/products/1'";
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(url);
-      setData(response.data);
-    } catch (error) {
-      console.error("Error during data fetching:", error);
-    }
-  };
 
   useEffect(() => {
     axios
-      .get("https://dummyjson.com/products/search?q=phone")
-      .then((response) => {
-        console.log(response.data);
-      });
+    .get("http://localhost:3000/shop"
+    ) // Perhatikan bahwa kami tidak menggunakan "/api" di sini
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
   }, []);
 
   return (
     <>
       <div className="App">
-        <header className="App-header">
-          <h1>Data Nama</h1>
-            {data.map((item, index) => (
-              <div key={index}>
-                Rendering item {index}
-                <span>
-                  {item.brand} = $ {item.price}
-                </span>
-              </div>
-            ))}
-        </header>
+        <header className="App-header"></header>
       </div>
     </>
   );
--- rajabisnis_backend/src/main.ts
+++ rajabisnis_backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set konfigurasi CORS
+  app.enableCors(); // Ini akan mengizinkan akses lintas asal dari semua origin
+
   await app.listen(3000);
 }
 bootstrap();
